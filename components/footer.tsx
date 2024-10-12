a557a95a457a3abe7770b5263f785d6841fbb2e1
JMC816 <jangmincheol816@gmail.com>
2024-10-13 05:07:45 +0900

    design: 하단바 컴포넌트 생성

---
+++ components/footer.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import {
+  CalendarDateRangeIcon,
+  CalendarIcon,
+  Cog6ToothIcon,
+} from "@heroicons/react/16/solid";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Footer() {
+  const pathname = usePathname();
+  return (
+    <div className="fixed bottom-0 flex justify-around w-full h-16 max-w-sm bg-neutral-700">
+      <div
+        className={`flex items-center justify-center w-full ${
+          pathname === "/" ? "bg-neutral-500" : "bg-neutral-700"
+        }`}
+      >
+        <Link href="/">
+          <CalendarDateRangeIcon className="w-11 " />
+        </Link>
+      </div>
+      <div
+        className={`flex items-center justify-center w-full ${
+          pathname === "/schedule" ? "bg-neutral-500" : "bg-neutral-700"
+        }`}
+      >
+        <Link href="/schedule">
+          <CalendarIcon className="w-11" />
+        </Link>
+      </div>
+      <div
+        className={`flex items-center justify-center w-full ${
+          pathname === "/set" ? "bg-neutral-500" : "bg-neutral-700"
+        }`}
+      >
+        <Link href="/set">
+          <Cog6ToothIcon className="w-11" />
+        </Link>
+      </div>
+    </div>
+  );
+}
