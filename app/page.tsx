dae17bdefcc032e924124065637a660ca8a91a74
JMC816 <jangmincheol816@gmail.com>
2024-10-17 23:56:30 +0900

    design: 스크롤 설정

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import { Calendar } from "@/components/ui/calendar";
 
 export default function ToDo() {
   return (
-    <>
-      <Calendar className="m-5 border rounded-md"></Calendar>
+    <div className="overflow-auto ">
+      <Calendar className="m-5 border rounded-md "></Calendar>
       <ToDoList></ToDoList>
-    </>
+    </div>
   );
 }
