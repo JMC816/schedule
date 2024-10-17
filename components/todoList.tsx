008fd84b078500bd60f62b0c0a9e09cd6911a126
JMC816 <jangmincheol816@gmail.com>
2024-10-18 01:13:37 +0900

    feat: todo 기능 생성

--- components/todoList.tsx
+++ components/todoList.tsx
@@ -1,5 +1,58 @@
+"use client";
+
+import { TrashIcon } from "@heroicons/react/16/solid";
+import { Button } from "./ui/button";
+import { Checkbox } from "./ui/checkbox";
+import { Input } from "./ui/input";
+import React, { useState } from "react";
+
 export default function ToDoList() {
+  const [value, setValue] = useState("");
+  const [toDo, setToDo] = useState<string[]>([]);
+
+  const getDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = (today.getMonth() + 1).toString().padStart(2, "0");
+    const day = today.getDate().toString().padStart(2, "0");
+    const dateString = year + "-" + month + "-" + day;
+    return dateString;
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setValue(value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setToDo((prev) => [...prev, value]);
+    setValue("");
+  };
   return (
-    <div className="h-full mb-5 ml-5 mr-5 border border-white rounded-md"></div>
+    <>
+      <div className="px-5 py-2 mb-[85px] ml-5 mr-5 border border-white rounded-md ">
+        <div className="mb-2 text-center">{getDate()}</div>
+        <form className="flex gap-1 mb-5" onSubmit={onSubmit}>
+          <Input onChange={onChange} value={value} />
+          <Button className="border border-white">입력</Button>
+        </form>
+        <div className="flex items-center justify-between mb-4 px-2h-7">
+          <Checkbox className="bg-white" />
+          <TrashIcon className="h-6 text-white" />
+        </div>
+        <div className="flex mb-4 border border-white rounded-md"></div>
+        {toDo.map((todos, index) => (
+          <div
+            key={index}
+            className="flex items-center justify-between h-5 gap-1 px-2 py-6 mb-5 bg-white border border-white rounded-md"
+          >
+            <Checkbox />
+            <span className="w-full pl-1 text-black">{todos}</span>
+            <TrashIcon className="h-6 text-black" />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
