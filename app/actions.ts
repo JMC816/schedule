0e53c1737ebc07777450fa8363439700b315ccc7
JMC816 <jangmincheol816@gmail.com>
2024-10-28 23:56:06 +0900

    feat: Todos 업로드

--- app/actions.ts
+++ app/actions.ts
@@ -1,18 +1,47 @@
 "use server";
 
 import db from "@/lib/db";
+import { revalidatePath } from "next/cache";
 
-export async function createToDos(date: string) {
-  const daydId = parseInt(date);
+export async function createToDo(date: string) {
+  const dayId = parseInt(date);
+  if (isNaN(dayId)) {
+    return;
+  }
   try {
-    const todos = await db.toDo.create({
-      data: {
-        id: daydId,
+    const existTodo = await db.toDo.findMany({
+      where: {
+        id: dayId,
       },
     });
-    console.log(todos.id);
+    if (existTodo.length === 0) {
+      await db.toDo.create({
+        data: {
+          id: dayId,
+        },
+      });
+    }
   } catch (error) {
     console.log(error);
   }
-  return null;
+}
+
+export async function uploadTodos(formdata: FormData, data: string) {
+  const text = formdata.get("todo") as string;
+  await db.toDos.create({
+    data: {
+      text,
+      toDoId: parseInt(data),
+    },
+  });
+  revalidatePath("/");
+}
+
+export async function getTodos() {
+  const todos = await db.toDos.findMany();
+  return todos;
+}
+
+export async function initializeTodaysTodo(today: string) {
+  await createToDo(today);
 }
