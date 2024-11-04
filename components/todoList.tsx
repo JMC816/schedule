a0fbb210b74f02d3b9d14506ad3808785220e236
JMC816 <jangmincheol816@gmail.com>
2024-11-04 10:40:23 +0900

    feat: 입력값 초기화

--- components/todoList.tsx
+++ components/todoList.tsx
@@ -4,7 +4,7 @@ import { TrashIcon } from "@heroicons/react/16/solid";
 import { Button } from "./ui/button";
 import { Checkbox } from "./ui/checkbox";
 import { Input } from "./ui/input";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useStore from "@/store";
 import { deleteTodos, initializeTodaysTodo, uploadTodos } from "@/app/actions";
 
@@ -18,6 +18,7 @@ export interface TodosProps {
 
 export default function ToDoList({ todos }: TodosProps) {
   const { date, setDate } = useStore((state) => state);
+  const [value, setValue] = useState<string>("");
 
   const formatDate = () => {
     const year = date.slice(0, 4);
@@ -43,8 +44,12 @@ export default function ToDoList({ todos }: TodosProps) {
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (value === "") {
+      return;
+    }
     const formData = new FormData(e.currentTarget);
     await uploadTodos(formData, date);
+    setValue("");
   };
 
   const onDelete = async (id: number) => {
@@ -54,12 +59,16 @@ export default function ToDoList({ todos }: TodosProps) {
       }
     });
   };
+
+  const onChage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
   return (
     <>
       <div className="px-5 py-2 mb-[85px] ml-5 mr-5 border border-white rounded-md ">
         <div className="mb-2 text-center">{formatDate()}</div>
         <form onSubmit={onSubmit} className="flex gap-1 mb-5">
-          <Input name="todo" type="text" />
+          <Input onChange={onChage} value={value} name="todo" type="text" />
           <Button className="border border-white">입력</Button>
         </form>
         <div className="flex items-center justify-between mb-4 px-2h-7">
