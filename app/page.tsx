6ede0c5766a7f6ff09db9061ba49c8ac678a44bc
JMC816 <jangmincheol816@gmail.com>
2024-11-09 01:43:16 +0900

    feat: completedTodos 전달

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import ToDoList from "@/components/todoList";
 import { Calendar } from "@/components/ui/calendar";
-import { getTodos } from "./actions";
+import { getCompletedTodos, getTodos } from "./actions";
 
 export default async function ToDo() {
   const todos = await getTodos();
+  const completedTodos = await getCompletedTodos();
   return (
     <div className="overflow-auto ">
       <Calendar
-        todos={todos}
+        completedTodos={completedTodos}
         className="flex justify-center m-5 border rounded-md "
       />
-      <ToDoList todos={todos} />
+      <ToDoList todos={todos} completedTodos={completedTodos} />
     </div>
   );
 }
