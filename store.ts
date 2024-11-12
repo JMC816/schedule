ea68b594733b202b8bcbb3ec4108d1b3089c2b25
JMC816 <jangmincheol816@gmail.com>
2024-11-12 10:11:02 +0900

    feat: Modal 상태 설정

--- store.ts
+++ store.ts
@@ -3,11 +3,15 @@ import { create } from "zustand";
 interface Store {
   date: string;
   setDate: (newDate: string) => void;
+  show: boolean;
+  toggleModal: () => void;
 }
 
 const useStore = create<Store>((set) => ({
   date: "",
   setDate: (newDate: string) => set({ date: newDate }),
+  show: false,
+  toggleModal: () => set((state) => ({ show: !state.show })),
 }));
 
 export default useStore;
