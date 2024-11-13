52ea1b69934649930b5ba343721ff9c2ac91c5dd
JMC816 <jangmincheol816@gmail.com>
2024-11-13 13:24:21 +0900

    design: todo 개수에 따른 배경색 타이틀

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,18 @@ export default async function ToDo() {
     <div className="overflow-auto ">
       <Calendar
         completedTodos={completedTodos}
-        className="flex justify-center m-5 border rounded-md "
+        className="flex justify-center mb-2 border rounded-md ml-7 mr-7 mt-7"
       />
+      <div className="flex justify-end gap-1 mr-7 ml-7">
+        <span className="text-xs">Less</span>
+        <div className="p-2 bg-green-100 rounded-sm"></div>
+        <div className="p-2 bg-green-300 rounded-sm"></div>
+        <div className="p-2 bg-green-500 rounded-sm"></div>
+        <div className="p-2 bg-green-700 rounded-sm"></div>
+        <div className="p-2 bg-green-900 rounded-sm"></div>
+        <div className="p-2 rounded-sm bg-green-950"></div>
+        <span className="text-xs">More</span>
+      </div>
       <ToDoList todos={todos} completedTodos={completedTodos} />
     </div>
   );
