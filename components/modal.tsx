e5057bb81daeec0de5415eb2f71381d8d34ee1fe
JMC816 <jangmincheol816@gmail.com>
2024-11-16 21:55:38 +0900

    refactor: 부모 모달과 자식 모달로 분리

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,48 +1,20 @@
 "use client";
 
-import { uploadTodos } from "@/app/actions";
-import useStore from "@/store";
-import { useEffect, useRef, useState } from "react";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
-import { motion, AnimatePresence } from "framer-motion";
+import { useModalStore } from "@/store";
+import ToDoModal from "./todoModal";
 
-interface Props {
+export interface Props {
   children: React.ReactNode;
 }
 
 export default function Modal({ children }: Props) {
-  const { show, toggleModal } = useStore();
-  const { date } = useStore();
-  const inputRef = useRef<HTMLInputElement | null>(null);
-  useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [show]);
-
-  const [value, setValue] = useState<string>("");
-
-  const onChage = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  };
-
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (value === "") {
-      return;
-    }
-    const formData = new FormData(e.currentTarget);
-    await uploadTodos(formData, date);
-    setValue("");
-    toggleModal();
-  };
+  const { todoModal, chartModal } = useModalStore();
   return (
     <>
       <div
         className="h-full"
         style={
-          show
+          todoModal || chartModal
             ? {
                 backgroundColor: "rgba(76,76,76,0.7)",
                 pointerEvents: "none",
@@ -60,35 +32,10 @@ export default function Modal({ children }: Props) {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            pointerEvents: show ? "auto" : "none",
+            pointerEvents: todoModal || chartModal ? "auto" : "none",
           }}
         >
-          <AnimatePresence>
-            {show && (
-              <motion.div
-                exit={{ opacity: 0, scale: 1 }}
-                initial={{ opacity: 0, scale: 0.95 }}
-                animate={{ opacity: 1, scale: 1 }}
-                className="z-10 flex flex-col items-center justify-center gap-3 px-4 py-4 ml-5 mr-5 rounded-lg bg-neutral-950"
-              >
-                <form onSubmit={onSubmit} className="flex flex-col gap-3">
-                  <Input
-                    onChange={onChage}
-                    value={value}
-                    name="todo"
-                    type="text"
-                    placeholder="할 일을 입력하세요."
-                    autoFocus
-                    ref={inputRef}
-                  />
-                  <Button className="w-48">입력</Button>
-                </form>
-                <Button onClick={toggleModal} className="w-48">
-                  취소
-                </Button>
-              </motion.div>
-            )}
-          </AnimatePresence>
+          {todoModal && <ToDoModal />}
         </div>
         {children}
       </div>
