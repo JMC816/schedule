b20b0728a4e32aef44de056c81b7c8d22d664f74
JMC816 <jangmincheol816@gmail.com>
2024-11-24 16:38:01 +0900

    feat: apexchart 생성 및 차트 데이터 입력

--- components/chartModal.tsx
+++ components/chartModal.tsx
@@ -1,11 +1,46 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import ChartHeader from "./chartHeader";
 import { motion, AnimatePresence } from "framer-motion";
-import { useModalStore } from "@/store";
+import { useChartStore, useModalStore } from "@/store";
 
 export default function ChartModal() {
   const { chartModal } = useModalStore();
+  const { chartData } = useChartStore();
+  const value = Object.values(chartData);
+  const chartOne = value.filter((n) => n == 1).length;
+  const chartTwo = value.filter((n) => n == 2).length;
+  const chartThree = value.filter((n) => n == 3).length;
+  const chartfour = value.filter((n) => n == 4).length;
+  const chartFive = value.filter((n) => n == 5).length;
+  const chartSixOver = value.filter((n) => n >= 6).length;
+
+  const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
+  const options = {
+    series: [
+      chartOne,
+      chartTwo,
+      chartThree,
+      chartfour,
+      chartFive,
+      chartSixOver,
+    ],
+    chartOptions: {
+      labels: ["one", "two", "three", "four", "five", "six"],
+      colors: [
+        "#dcfce7",
+        "#86efac",
+        "#22c55e",
+        "#15803d",
+        "#14532d",
+        "#052e16",
+      ],
+      stroke: {
+        colors: undefined,
+      },
+    },
+  };
 
   return (
     <AnimatePresence>
@@ -19,7 +54,13 @@ export default function ChartModal() {
         >
           <ChartHeader />
           <div className="flex flex-col gap-4 ml-7 mr-7 mt-[85px]">
-            <div className="rounded-xl bg-neutral-700"></div>
+            <div className="rounded-xl bg-neutral-700">
+              <ApexChart
+                type="donut"
+                options={options.chartOptions}
+                series={options.series}
+              />
+            </div>
             <div className="rounded-md bg-neutral-700"></div>
           </div>
         </motion.div>
