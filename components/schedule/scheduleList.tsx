8f5a6831f6bf6e9cd468fe75c28a108af9e82fe2
JMC816 <jangmincheol816@gmail.com>
2024-11-30 16:49:26 +0900

    feat: 날짜 미 클릭 시 팝업창 오픈

--- components/schedule/scheduleList.tsx
+++ components/schedule/scheduleList.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useModalStore } from "@/store";
-import { PlusIcon } from "@heroicons/react/16/solid";
+import { CalendarIcon, PlusIcon } from "@heroicons/react/16/solid";
 import { DateRange } from "react-day-picker";
 
 interface RangeProps {
@@ -10,20 +10,31 @@ interface RangeProps {
 
 export default function ScheduleList({ range }: RangeProps) {
   const { changeModalState } = useModalStore();
+  const onPopupClick = () => {
+    if (!range?.from || !range?.to) {
+      changeModalState("schedulePopup");
+    }
+  };
   return (
     <>
-      <div className="px-5 py-2 mb-2 ml-5 mr-5 border border-white rounded-md ">
+      <div className="px-2 py-2 mb-2 ml-5 mr-5 ">
         <div className="flex gap-4 h-7">
-          <div className="w-full text-center border border-white rounded-md">
+          <div className="flex items-center w-full gap-2 text-center border border-white rounded-md">
+            <CalendarIcon className="h-full p-1" />
             {range?.from?.toLocaleDateString()}
           </div>
-          <div className="w-full text-center border border-white rounded-md">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-4 border rounded-lg"></div>
+            <div />
+          </div>
+          <div className="flex items-center w-full gap-2 text-center border border-white rounded-md ">
+            <CalendarIcon className="h-full p-1" />
             {range?.to?.toLocaleDateString()}
           </div>
         </div>
       </div>
       <div
-        onClick={() => changeModalState("chartModal")}
+        onClick={onPopupClick}
         className="flex self-center justify-center ml-5 mr-5 rounded-md bg-neutral-700 "
       >
         <PlusIcon className="w-12" />
