818b177ecfdd10123bb33970f00f374ca6edfd0f
JMC816 <jangmincheol816@gmail.com>
2024-11-30 21:14:56 +0900

    feat: 아이콘 클릭 시 페이지 조건 추가

--- components/header.tsx
+++ components/header.tsx
@@ -1,32 +1,36 @@
 "use client";
 
 import { useModalStore } from "@/store";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
   const { changeModalState } = useModalStore();
+  const pathname = usePathname();
   return (
     <div className="fixed top-0 flex items-center w-full min-h-16 bg-neutral-700">
       <div className="flex justify-end w-full mr-4">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="cursor-pointer size-8"
-          onClick={() => changeModalState("chartModal")}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z"
-          />
-        </svg>
+        {pathname === "/" && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="cursor-pointer size-8"
+            onClick={() => changeModalState("chartModal")}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z"
+            />
+          </svg>
+        )}
       </div>
     </div>
   );
