f03af779a7b732d3e903bd7459359c2c9f39f272
JMC816 <jangmincheol816@gmail.com>
2024-12-21 21:29:41 +0900

    chore: 리스트 생성 버튼 삭제

--- components/schedule/scheduleCreateBtn.tsx
+++ components/schedule/scheduleCreateBtn.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import { useModalStore, useRangeStore } from "@/store";
-import { CalendarIcon, PlusIcon } from "@heroicons/react/16/solid";
+import { useRangeStore } from "@/store";
+import { CalendarIcon } from "@heroicons/react/16/solid";
 
 export default function ScheduleCreateBtn() {
-  const { changeModalState } = useModalStore();
   const { range } = useRangeStore();
-  const onPopupClick = () => {
-    if (!range?.from || !range?.to) {
-      changeModalState("schedulePopup");
-    } else {
-      changeModalState("scheduleModal");
-    }
-  };
+
   return (
     <>
       <div className="px-2 py-2 mb-2 ml-5 mr-5 ">
@@ -30,12 +23,6 @@ export default function ScheduleCreateBtn() {
             {range?.to?.toLocaleDateString()}
           </div>
         </div>
-      </div>
-      <div
-        onClick={onPopupClick}
-        className="flex items-center justify-center py-2 rounded-md ml-7 mr-7 bg-neutral-700 "
-      >
-        <PlusIcon className="w-10" />
       </div>
     </>
   );
