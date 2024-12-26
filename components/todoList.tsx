0dd66c551496f1f3e23b9e0662f9f67f116c6b43
JMC816 <jangmincheol816@gmail.com>
2024-12-26 18:34:59 +0900

    refactor: 날짜 UI 및 TODO, COMPLETED UI 스타일

--- components/todoList.tsx
+++ components/todoList.tsx
@@ -7,15 +7,15 @@ import {
   deleteCompletedTodos,
   deleteTodos,
   getCompletedTodos,
-  initializeTodaysTodo,
 } from "@/app/actions";
 import { motion, AnimatePresence } from "framer-motion";
-import { useChartStore, useStore } from "@/store";
+import { useChartStore, useStore, useToDoListStore } from "@/store";
 import { CheckedProps, ToDosProps } from "./todo";
 
 export default function ToDoList({ todos, completedTodos }: ToDosProps) {
-  const { date, setDate } = useStore();
+  const { date } = useStore();
   const { setChartData } = useChartStore();
+  const { checkToDo, setCheckToDo } = useToDoListStore();
 
   useEffect(() => {
     const formatChartDate = (month: number) => {
@@ -54,18 +54,6 @@ export default function ToDoList({ todos, completedTodos }: ToDosProps) {
     updateChartData();
   }, [completedTodos]);
 
-  useEffect(() => {
-    if (!date) {
-      const today = new Date()
-        .toLocaleDateString()
-        .replace(/\./g, "")
-        .split(" ")
-        .join("");
-      setDate(today);
-      initializeTodaysTodo(today);
-    }
-  }, [date, setDate]);
-
   const filteredTodos = todos.filter(
     (todos) => todos.toDoId === parseInt(date)
   );
@@ -93,57 +81,89 @@ export default function ToDoList({ todos, completedTodos }: ToDosProps) {
     });
   };
 
+  const weeks = [
+    "일요일",
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일",
+  ];
+
+  const formatDayWeeks =
+    date.substring(0, 4) +
+    "/" +
+    date.substring(4, 6) +
+    "/" +
+    date.substring(6, 8);
+
+  const dayWeeks = new Date(formatDayWeeks).getDay();
+
   return (
     <>
-      <div className="flex justify-between gap-2 py-2 mt-20 rounded-md ml-7 mr-7">
-        {filteredTodos.length === 0 ? (
-          <>
-            <div className="w-full p-2 text-center rounded-3xl bg-neutral-500 active:bg-neutral-700">
-              <span>TO DO</span>
-            </div>
-          </>
-        ) : (
-          filteredTodos.map((todos) => (
-            <AnimatePresence key={todos.id}>
-              <motion.div
-                layout
-                className="flex items-center justify-between px-2 py-4 mb-5 rounded-md bg-neutral-700"
-              >
-                <input type="checkbox" onChange={() => onCheckedTodos(todos)} />
-                <span className="w-full pl-3 text-white">{todos.text}</span>
-                <TrashIcon
-                  className="h-6 text-white cursor-pointer"
-                  onClick={() => onDeleteTodos(todos.id)}
-                />
-              </motion.div>
-            </AnimatePresence>
-          ))
-        )}
-        {filteredCompletedTodos.length === 0 ? (
-          <>
-            <div className="w-full p-2 text-center rounded-3xl bg-neutral-500 active:bg-neutral-700">
-              <span>COMPLETED</span>
-            </div>
-          </>
-        ) : (
-          filteredCompletedTodos.map((todos) => (
-            <AnimatePresence key={todos.id}>
-              <motion.div
-                layout
-                className="flex items-center justify-between gap-1 px-2 py-4 mb-5 rounded-md bg-neutral-800"
-              >
-                <input type="checkbox" checked={true} disabled />
-                <span className="w-full pl-3 text-gray-500 line-through">
-                  {todos.text}
-                </span>
-                <TrashIcon
-                  className="h-6 text-white cursor-pointer"
-                  onClick={() => onDeleteCompletedTodos(todos.id)}
-                />
-              </motion.div>
-            </AnimatePresence>
-          ))
-        )}
+      <div className="overflow-auto ">
+        <div className="flex flex-col mt-20 ml-7 mr-7">
+          <span className="text-4xl">{date.substring(6)}</span>
+          <span>{weeks[dayWeeks]}</span>
+          <div className="mt-1 mb-4 border"></div>
+        </div>
+        <div className="flex justify-between gap-2 mb-4 ml-7 mr-7">
+          <div
+            onClick={() => setCheckToDo(true)}
+            className={`w-full p-2 text-center rounded-3xl ${
+              checkToDo ? "bg-neutral-500" : "bg-neutral-700"
+            }`}
+          >
+            <span>TO DO</span>
+          </div>
+          <div
+            onClick={() => setCheckToDo(false)}
+            className={`w-full p-2 text-center rounded-3xl ${
+              checkToDo ? "bg-neutral-700" : "bg-neutral-500"
+            }`}
+          >
+            <span>COMPLETED</span>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center mb-24 ml-7 mr-7">
+          {!checkToDo
+            ? filteredTodos.map((todos) => (
+                <AnimatePresence key={todos.id}>
+                  <motion.div
+                    layout
+                    className="flex w-full px-2 py-4 mb-4 rounded-md bg-neutral-700"
+                  >
+                    <input
+                      type="checkbox"
+                      onChange={() => onCheckedTodos(todos)}
+                    />
+                    <span className="w-full pl-3 text-white">{todos.text}</span>
+                    <TrashIcon
+                      className="h-6 text-white cursor-pointer"
+                      onClick={() => onDeleteTodos(todos.id)}
+                    />
+                  </motion.div>
+                </AnimatePresence>
+              ))
+            : filteredCompletedTodos.map((todos) => (
+                <AnimatePresence key={todos.id}>
+                  <motion.div
+                    layout
+                    className="flex w-full px-2 py-4 mb-4 rounded-md bg-neutral-800"
+                  >
+                    <input type="checkbox" checked={true} disabled />
+                    <span className="w-full pl-3 text-gray-500 line-through">
+                      {todos.text}
+                    </span>
+                    <TrashIcon
+                      className="h-6 text-white cursor-pointer"
+                      onClick={() => onDeleteCompletedTodos(todos.id)}
+                    />
+                  </motion.div>
+                </AnimatePresence>
+              ))}
+        </div>
       </div>
     </>
   );
