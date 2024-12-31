84c025b568d83253ece0bc633e3f424935e5b806
JMC816 <jangmincheol816@gmail.com>
2024-12-31 14:36:28 +0900

    feat: 시작페이지로 변경 및 제작

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-import { getCompletedTodos, getTodos } from "./actions";
-import ToDo_Box from "@/components/todo";
+import Image from "next/image";
+import Link from "next/link";
 
-export default async function ToDo() {
-  const todos = await getTodos();
-  const completedTodos = await getCompletedTodos();
-  return <ToDo_Box todos={todos} completedTodos={completedTodos} />;
+export default function StartPage() {
+  return (
+    <div className="ml-[20%] mr-[20%] flex flex-col justify-around items-center h-[100vh]">
+      <Image src="/icons/512.png" alt="이미지" width={200} height={200} />
+      <Link
+        href="/login"
+        className="flex justify-center w-full py-2 rounded-full bg-neutral-500"
+      >
+        <span>시작하기</span>
+      </Link>
+    </div>
+  );
 }
