f90f6cef410d03ad76010fd9268cd52c6e7f40ac
JMC816 <jangmincheol816@gmail.com>
2025-01-04 23:41:26 +0900

    feat: useFormState로 회원가입 결과 UI로 전달

--- app/(auth)/create-account/page.tsx
+++ app/(auth)/create-account/page.tsx
@@ -1,26 +1,41 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import FormButton from "@/components/form/form-btn";
+import FormInput from "@/components/form/form-input";
 import Link from "next/link";
+import { useFormState } from "react-dom";
+import { createAccount } from "./actions";
 
 export default function Create_Account() {
+  const [state, dispatch] = useFormState(createAccount, null);
   return (
     <div className="ml-[20%] mr-[20%] h-[100vh] justify-center items-center gap-2 flex flex-col">
-      <form className="flex flex-col w-full gap-2 p-5 bg-neutral-700 rounded-xl">
-        <input
-          placeholder="이메일을 입력하세요"
-          type="text"
-          className="p-1 text-black rounded-sm"
+      <form
+        action={dispatch}
+        className="flex flex-col w-full gap-2 p-5 bg-neutral-700 rounded-xl"
+      >
+        <FormInput
+          name="email"
+          required
+          type="email"
+          placeholder="이메일"
+          errors={state?.fieldErrors.email}
         />
-        <input
-          placeholder="아이디를 입력하세요"
-          type="text"
-          className="p-1 text-black rounded-sm"
+        <FormInput
+          name="id"
+          required
+          type="id"
+          placeholder="아이디"
+          errors={state?.fieldErrors.id}
         />
-        <input
-          placeholder="비밀번호를 입력하세요"
+        <FormInput
+          name="password"
+          required
           type="password"
-          className="p-1 text-black rounded-sm"
+          placeholder="비밀번호"
+          errors={state?.fieldErrors.password}
         />
-        <Button>회원가입</Button>
+        <FormButton text="회원가입" />
       </form>
       <Link href="/login">
         <span className="text-gray-500 underline">계정이 있으신가요?</span>
