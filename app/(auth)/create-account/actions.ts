be7c18906784c0381a7ce2a52dca68f2772136ac
JMC816 <jangmincheol816@gmail.com>
2025-01-04 22:17:58 +0900

    feat: zod로 email,id,password 검증

---
+++ app/(auth)/create-account/actions.ts
@@ -0,0 +1,32 @@
+"use server";
+import { PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constans";
+import { z } from "zod";
+
+const formScheam = z.object({
+  id: z
+    .string({
+      invalid_type_error: "아이디는 문자형태 입니다.",
+      required_error: "아이디는 필수 입니다.",
+    })
+    .min(3, "너무 짧습니다.")
+    .toLowerCase()
+    .trim()
+    .max(10, "너무 깁니다."),
+  email: z.string().email().trim().toLowerCase(),
+  password: z.string().min(10).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+});
+
+export async function createAccount(prevState: any, formData: FormData) {
+  const data = {
+    email: formData.get("email"),
+    id: formData.get("id"),
+    password: formData.get("password"),
+  };
+  const result = formScheam.safeParse(data);
+  if (!result.success) {
+    console.log(result.error.flatten());
+    return result.error.flatten();
+  } else {
+    console.log(result.data);
+  }
+}
