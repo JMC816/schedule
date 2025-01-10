5f2730f6f336d72121cc6e8572aa32898fa417f7
JMC816 <jangmincheol816@gmail.com>
2025-01-10 18:12:42 +0900

    feat: username, email 유무 서버 컴포넌트 생성

--- app/(auth)/create-account/actions.ts
+++ app/(auth)/create-account/actions.ts
@@ -1,6 +1,39 @@
 "use server";
 import { PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constans";
+import db from "@/lib/db";
 import { z } from "zod";
+
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (user) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (user) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
 const formScheam = z.object({
   username: z
@@ -11,8 +44,14 @@ const formScheam = z.object({
     .min(3, "너무 짧습니다.")
     .toLowerCase()
     .trim()
-    .max(10, "너무 깁니다."),
-  email: z.string().email().trim().toLowerCase(),
+    .max(10, "너무 깁니다.")
+    .refine(checkUniqueUsername, "이미 닉네임이 존재합니다."),
+  email: z
+    .string()
+    .email()
+    .trim()
+    .toLowerCase()
+    .refine(checkUniqueEmail, "이미 메일이 존재합니다."),
   password: z.string().min(10).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
 });
 
@@ -22,7 +61,7 @@ export async function createAccount(prevState: any, formData: FormData) {
     email: formData.get("email"),
     password: formData.get("password"),
   };
-  const result = formScheam.safeParse(data);
+  const result = await formScheam.safeParseAsync(data);
   if (!result.success) {
     console.log(result.error.flatten());
     return result.error.flatten();
