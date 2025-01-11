c3cf24905d044165f2b533518f138d13351d60c5
JMC816 <jangmincheol816@gmail.com>
2025-01-11 22:23:58 +0900

    feat: 입력한 비밀번호와 DB에 저장된 비밀번호 비교

--- app/(auth)/login/actions.ts
+++ app/(auth)/login/actions.ts
@@ -1,14 +1,34 @@
 "use server";
 
+import bcrypt from "bcrypt";
 import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from "@/lib/constans";
+import db from "@/lib/db";
 import { z } from "zod";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return Boolean(user);
+};
 
 const formSchema = z.object({
-  username: z.string({ required_error: "넥네임은 필수입니다." }).toLowerCase(),
+  email: z
+    .string()
+    .email()
+    .toLowerCase()
+    .refine(checkEmailExists, "이메일이 존재하지 않습니다."),
   password: z
     .string({ required_error: "비밀번호는 필수입니다." })
     .min(PASSWORD_MIN_LENGTH)
@@ -17,13 +37,38 @@ const formSchema = z.object({
 
 export async function login(prevState: any, formData: FormData) {
   const data = {
-    username: formData.get("username"),
+    email: formData.get("email"),
     password: formData.get("password"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+    const ok = await bcrypt.compare(
+      result.data.password,
+      user!.password ?? "xxxx"
+    );
+    if (ok) {
+      const session = await getSession();
+      session.id = user!.id;
+      await session.save();
+      redirect("/todo");
+    } else {
+      return {
+        fieldErrors: {
+          password: ["비밀번호가 일치하지 않습니다."],
+          email: [],
+        },
+      };
+    }
   }
 }
