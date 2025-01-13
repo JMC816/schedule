5b8562d880affe97671414f2b125dab223d639b3
JMC816 <jangmincheol816@gmail.com>
2025-01-13 21:31:02 +0900

    design: 로그아웃 페이지 퍼블리싱

--- app/(main)/set/page.tsx
+++ app/(main)/set/page.tsx
@@ -1,8 +1,19 @@
-export default function Set() {
+import { getUser, logOut } from "./actions";
+
+export default async function Set() {
+  const user = await getUser();
   return (
     <div className="overflow-hidden ">
       <div className=" mt-[85px] mr-5 ml-5">
         <div className="flex flex-col gap-2">
+          <div className="flex justify-between p-4 rounded-lg bg-neutral-700">
+            <span>{user?.username}</span>
+          </div>
+          <div className="flex justify-between p-4 rounded-lg bg-neutral-700">
+            <form action={logOut}>
+              <button>로그아웃</button>
+            </form>
+          </div>
           <div className="flex justify-between p-4 rounded-lg bg-neutral-700">
             <span>앱 버전</span>
             <span className="text-gray-500">v1.0.0</span>
