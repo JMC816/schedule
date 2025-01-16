96b7f292e386938dc4471edca242990e856f09d6
JMC816 <jangmincheol816@gmail.com>
2025-01-17 00:07:08 +0900

    chore: manifest.ts 파일 수정

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,14 +6,17 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: "더 스케줄러",
     description: "오늘 할 일과 일정을 달력에 기록할 수 있는 서비스",
     start_url: "/",
+    scope: "/",
     display: "standalone",
-    background_color: "#000000",
-    theme_color: "#000000",
+    background_color: "#FFFFFF",
+    theme_color: "#FFFFFF",
+    lang: "ko-KR",
     icons: [
       {
         src: "/icons/192.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "maskable",
       },
       {
         src: "/icons/512.png",
