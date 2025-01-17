31b16c94e6584577b94501f5cb3d58d3cd49e998
JMC816 <jangmincheol816@gmail.com>
2025-01-17 16:20:38 +0900

    design: 아이콘 색상 반전

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,13 @@ import Link from "next/link";
 export default function StartPage() {
   return (
     <div className="ml-[20%] mr-[20%] flex flex-col justify-around items-center h-[100vh]">
-      <Image src="/icons/512.png" alt="이미지" width={200} height={200} />
+      <Image
+        src="/icons/512.png"
+        alt="이미지"
+        width={100}
+        height={100}
+        className="invert"
+      />
       <Link
         href="/login"
         className="flex justify-center w-full py-2 rounded-full bg-neutral-500"
